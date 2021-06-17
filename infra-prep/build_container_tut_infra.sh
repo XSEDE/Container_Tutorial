60c726082cffbc9a7cff1b08496d8d75e296b614
Eric Coulter <jecoulte@iu.edu>
2021-06-17 16:08:24 -0400

    Updated for working tests
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- infra-prep/build_container_tut_infra.sh
+++ infra-prep/build_container_tut_infra.sh
@@ -8,13 +8,15 @@
 #    about any code that is not well commented or is unclear.
 #    Or, is at the least, willing to gamble.
 
+echo 'Creating Container Tutorial infrasturcture with hardcoded values!'
+
 openrc_path="/home/jecoulte/Work/Tools/Jetstream_cli/openrc-files/openrc-staff.sh"
 
 cluster_name="jec-tuttest"
 volume_size=5
 headnode_size="m1.small"
 
-current_tutorial="Gateways2020"
+current_tutorial="SGCI2021"
 
 source ${openrc_path}
 
@@ -24,13 +26,13 @@ if [[ ! -f ${openrc_path} ]]; then
 fi
 
 #Build a cluster:
-if [[ -f ./tut_VC/cluster_create.sh ]]; then
+if [[ -f ./${current_tutorial}_VC/cluster_create.sh ]]; then
   echo "Using existing clone of the VC repo"
 else
-  git clone --single-branch --branch cent8-update https://github.com/XSEDE/CRI_Jetstream_Cluster tut_VC
+  git clone --single-branch --branch cent8-update https://github.com/XSEDE/CRI_Jetstream_Cluster ${current_tutorial}_VC
 fi
   
-cd ./tut_VC
+cd ./${current_tutorial}_VC
   
 ./cluster_create.sh -n ${cluster_name} -s ${headnode_size} -v ${volume_size} -o ${openrc_path} -d > cluster_build_output.txt
 
@@ -49,28 +51,40 @@ else
   done
 fi
 
-scp user_list.txt centos@${cluster_ip}:
 
 ssh centos@${cluster_ip} 'for user in train{01..99}; do sudo useradd $user; done'
 ssh centos@${cluster_ip} 'for user in train{01..99}; do sudo usermod -aG docker $user; done'
-#What about needing sudo privileges with Singularity?!
+
+#set user passwords!
+scp user_list.txt centos@${cluster_ip}:
 ssh centos@${cluster_ip} 'cat user_list.txt | sudo chpasswd'
 
-# NEED TO SPIT OUT FILES FOR THE CONTAINER TUTORIAL LIKE dice.py, etc.
-ssh centos@${cluster_ip} 'sudo mkdir -p -m 755 /opt/ohpc/pub/examples'
-scp ./${current_tutorial}/files/* centos@${cluster_ip}:/opt/ohpc/pub/examples/
+#Allow sudo + singularity useage for building
+scp singularity-sudoers centos@${cluster_ip}:
+ssh centos@${cluster_ip} 'sudo cp singularity-sudoers /etc/sudoers.d/'
+
+# Copy over files used in the exercises
+ssh centos@${cluster_ip} 'sudo mkdir -p -m 777 /opt/ohpc/pub/examples'
+scp ../${current_tutorial}/files/* centos@${cluster_ip}:/opt/ohpc/pub/examples/
 
 #Now, create a script to test the commands in the current tutorial directory:
 # At least for Day 1...
-cd ${current_tutorial}
-echo "#!/bin/bash" > EX_test_script.sh
-grep -hE '^\$|```\$' Day1\ Ex\ 1\ Part\ * | tr -d '[\`\$]'  | sed 's/^ //' >> EX_test_script
-chmod u+x EX_test_script.sh
+echo "#!/bin/bash" > ${current_tutorial}_test.sh
+grep -hE '^\$|```\$' ../${current_tutorial}/Day1\ Ex\ 1\ Part\ * | tr -d '[\`]'  | sed 's/^\$ //' | grep -vE "singularity remote|singularity push" >> ${current_tutorial}_test.sh
 
-scp EX_test_script.sh centos@${cluster_ip}:
+#Fixes for specific problematic commands!
+sed -i 's@echo 5 | docker run -i $USER/py3-dice@docker run -i $USER/py3-dice@' ${current_tutorial}_test.sh
+
+sed -i 's/vim \.\/Dice.def/sed -i "s@\\\$USERNAME@\$USER@" Dice.def/' ${current_tutorial}_test.sh
+
+sed -i 's/vim/#vim/' ${current_tutorial}_test.sh
+
+chmod u+x ${current_tutorial}_test.sh
+
+scp ${current_tutorial}_test.sh centos@${cluster_ip}:
 
 echo "Running commands from EX1 now!"
 
-ssh centos@${cluster_ip} 'sudo cp EX_test_script.sh /home/train99/ && sudo chown train99:train99 /home/train99/EX_test_script.sh && sudo su - train99 -c "/home/train99/EX_test_script.sh > EX_test_output.txt" && sudo cp /home/train99/EX_test_output.txt /home/centos/'
+ssh centos@${cluster_ip} "sudo cp ${current_tutorial}_test.sh /home/train99/ && sudo chown train99:train99 /home/train99/${current_tutorial}_test.sh && sudo su - train99 -c \"/home/train99/${current_tutorial}_test.sh > ${current_tutorial}_test_out.txt\" && sudo cp /home/train99/${current_tutorial}_test_out.txt /home/centos/"
 
-scp centos@${cluster_ip}:EX_test_output.txt ./
+scp centos@${cluster_ip}:${current_tutorial}_test_out.txt ./
